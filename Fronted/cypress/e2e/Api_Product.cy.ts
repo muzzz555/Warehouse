887bcd650a9334bc5abd3e5ccf72bf2ebb0dc655
muzzz555 <53386198+muzzz555@users.noreply.github.com>
2023-05-21 13:50:16 +0700

    Update_Test_GET_API

--- Fronted/cypress/e2e/Api_Product.cy.ts
+++ Fronted/cypress/e2e/Api_Product.cy.ts
@@ -1,44 +1,81 @@
 class AllStock {
   status?: Number;
-  body?: [];
+  body?: any;
 }
 
-describe("template spec", () => {
-  it("passes", () => {
+class Stock {
+  status?: Number;
+  body?: any;
+}
+
+const expectedResponse = [
+  "id",
+  "name",
+  "image",
+  "price",
+  "stock",
+  "createdAt",
+  "updatedAt",
+];
+
+describe("Test Case Api", () => {
+  it("API_Get_Products", () => {
     const option = {
       method: "GET",
       url: "http://localhost:8085/api/v2/stock/product",
     };
     cy.request(option).as("response");
-
     cy.get<AllStock>("@response").should((res) => {
       expect(res.status).eq(200);
-      expect(res)
-        .to.have.nested.property("body[0]")
-        .that.includes.all.keys([
+      res.body.forEach((item: any) => {
+        expect(item).to.have.all.keys(
           "id",
           "name",
           "image",
           "price",
           "stock",
           "createdAt",
-          "updatedAt",
-        ]);
+          "updatedAt"
+        );
+      });
     });
   });
-});
 
-// it('passes', () => {
-//   const option = {
-//     method:"GET",
-//     url:"http://localhost:8085/api/v2/stock/product"
-//   }
-//   cy.request(option).as('response')
+  it("API_Get_Products_By_Id", () => {
+    const option = {
+      method: "GET",
+      url: "http://localhost:8085/api/v2/stock/product/101",
+    };
+    cy.request(option).as("response");
+    cy.get<Stock>("@response").should((res) => {
+      expect(res.status).eq(200);
+      const actualResponse = Object.keys(res.body);
+      expect(actualResponse.length).to.eq(7);
+      expectedResponse.forEach((property) => {
+        expect(actualResponse).to.include(property);
+      });
+    });
+  });
 
-//   cy.get('@response').should((res) => {
-//     expect(res.status).eq(200)
-//     expect(res.body).to.deep.eq({
-//     })
-//   })
-// })
-
+  it("API_Get_Products_By_Keyword", () => {
+    const option = {
+      method: "GET",
+      url: "http://localhost:8085/api/v2/stock/product/keyword/StockName05",
+    };
+    cy.request(option).as("response");
+    cy.get<Stock>("@response").should((res) => {
+      expect(res.status).eq(200);
+      res.body.forEach((item: any) => {
+        expect(item).to.have.all.keys(
+          "id",
+          "name",
+          "image",
+          "price",
+          "stock",
+          "createdAt",
+          "updatedAt"
+        );
+      });
+    });
+  });
+})
